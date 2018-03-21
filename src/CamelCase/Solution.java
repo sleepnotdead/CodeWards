9b390e903e795889cfaa0971cad15137b1826863
mborisovsky <mborisovsky@determine.com>
2018-03-21 18:35:09 +0200

    CamelCase task solution

---
+++ src/CamelCase/Solution.java
@@ -0,0 +1,20 @@
+package CamelCase;
+
+/**
+ * Created by admin on 3/21/18.
+ */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.lang.StringBuilder;
+
+class Solution{
+
+    static String toCamelCase(String s){
+        Matcher m = Pattern.compile("[_|-](\\w)").matcher(s);
+        StringBuffer sb = new StringBuffer();
+        while (m.find()) {
+            m.appendReplacement(sb, m.group(1).toUpperCase());
+        }
+        return m.appendTail(sb).toString();
+    }
+}
